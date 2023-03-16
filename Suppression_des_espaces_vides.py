f60606345aa53dc1df967efbed6fddd67c38f3b8
Dany Dubé <74633244+danydube1971@users.noreply.github.com>
2023-03-16 19:46:28 -0400

    Delete Suppression_des_espaces_vides.py

--- Suppression_des_espaces_vides.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Remplacement des espaces par des underscores dans les noms de fichier dans le dossier courant et sous-dossiers
-for root, dirs, files in os.walk('.'):
-    for file in files:
-        if ' ' in file:
-            os.rename(os.path.join(root, file), os.path.join(root, file.replace(' ', '_')))
-
